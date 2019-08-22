bd41e03bc9aabe8436c5c8e2eda6659df6aa7b2e
nabil-e <nabilelassal@gmail.com>
2019-08-22 21:30:28 +0200

    add mapBox to search lat/long

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const request = require('request')
 const unit = 'auto' // us for F°
 const lang = 'fr' // change the language
+const limite = 1
 
-const url = `https://api.darksky.net/forecast/5787f0a2586effc6189a13dc45d00f27/50.6333,5.8?units=${unit}&lang=${lang}`
+const map = `https://api.mapbox.com/geocoding/v5/mapbox.places/Liège-herve.json?access_token=pk.eyJ1IjoibmFiaWwtZSIsImEiOiJjanptdmtzd2IwZ2dsM2hveGphcm92cHNsIn0.77bsmqQ5yeccnT1pNXtzaA&language=${lang}&limit=${limite}`
 
-request({url: url, json: true}, (err, res) => {
+request({url: map, json: true}, (err, res) => {
+    const latlon = res.body.features[0].center
+    const url = `https://api.darksky.net/forecast/5787f0a2586effc6189a13dc45d00f27/${latlon[1]},${latlon[0]}?units=${unit}&lang=${lang}`
+    
+    request({url: url, json: true}, (error, response) => {
+            const resp = response.body.currently
+            console.log(res.body.features[0].place_name)
+            console.log('--------------------------------\n')
+            console.log(`${response.body.daily.data[0].summary}\nIl fait ${resp.temperature} degrés.\nIl y a ${resp.precipProbability}% de chance de précipitation.`)        
+    })
+})
+ 
+// console.log(req.lat)
+
+
+
     // const data = JSON.parse(res.body)
     // console.log(res.body.currently)
-    const resp = res.body.currently
-        console.log(`${res.body.daily.data[0].summary}\nIt is currently ${resp.temperature} degrees out.\nThere is ${resp.precipProbability}% chance of rain`)
-        
-})
+    
+    // console.log(res.body.features[0].center[1])    
